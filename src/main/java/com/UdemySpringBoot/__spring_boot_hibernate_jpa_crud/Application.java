ce43f4e47c36c9ed642a8fcb331fbe60218c8ebc
RonanD653 <Ronan_D@Live.co.uk>
2024-08-15 16:01:49 +0100

    Initial commit

---
+++ src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/Application.java
@@ -0,0 +1,13 @@
+package com.UdemySpringBoot.__spring_boot_hibernate_jpa_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
