ffd827dcc56b7c1a29a9334f6f5d7c923c994db2
RonanD653 <Ronan_D@Live.co.uk>
2024-09-09 16:02:41 +0100

    CRUD

--- src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/Application.java
+++ src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/Application.java
@@ -18,8 +18,41 @@ public class Application {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+			//createMultipleStudents(studentDAO);
+			readStudent(studentDAO);
 		};
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+		System.out.println("Creating new student object...");
+
+		Student tempStudent = new Student("Daffy", "Duck", "Daffy@mail.com");
+		studentDAO.save(tempStudent);
+
+		int id = tempStudent.getId();
+		System.out.println("Saved student. Genertated Id: " + id);
+
+		System.out.println("Retrieving student with id " + id);
+		Student myStudent = studentDAO.findById(id);
+
+		System.out.println("Student: " + myStudent);
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		System.out.println("Creating 3 new student object...");
+		Student tempStudent1 = new Student("john", "Doe", "John@mail.com");
+		Student tempStudent2 = new Student("Mary", "Public", "Mary@mail.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "Bonita@mail.com");
+
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+		System.out.println("Saved student. Generate ID: " + tempStudent1.getId());
+		System.out.println("Saved student. Generate ID: " + tempStudent2.getId());
+		System.out.println("Saved student. Generate ID: " + tempStudent3.getId());
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
--- src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/dao/StudentDAO.java
+++ src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/dao/StudentDAO.java
@@ -5,4 +5,6 @@ import com.UdemySpringBoot.__spring_boot_hibernate_jpa_crud.entity.Student;
 public interface StudentDAO {
 
     void save(Student student);
+
+    Student findById(Integer id);
 }
