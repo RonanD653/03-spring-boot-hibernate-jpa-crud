40fe253d4fe47185de62facf984827554c972f6d
RonanD653 <Ronan_D@Live.co.uk>
2024-09-10 16:34:42 +0100

    JPA queries

--- src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/dao/StudentDAO.java
+++ src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/dao/StudentDAO.java
@@ -2,9 +2,15 @@ package com.UdemySpringBoot.__spring_boot_hibernate_jpa_crud.dao;
 
 import com.UdemySpringBoot.__spring_boot_hibernate_jpa_crud.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student student);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
 }
--- src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/dao/StudentDAOImpl.java
+++ src/main/java/com/UdemySpringBoot/__spring_boot_hibernate_jpa_crud/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.UdemySpringBoot.__spring_boot_hibernate_jpa_crud.dao;
 
 import com.UdemySpringBoot.__spring_boot_hibernate_jpa_crud.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -26,5 +29,18 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+        theQuery.setParameter("theData", lastName);
+        return theQuery.getResultList();
     }
 }
